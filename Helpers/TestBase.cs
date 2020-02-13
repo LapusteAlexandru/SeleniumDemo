e5f386a8ad92f47a622933c0b828142fcc6dd5fc
alexandru.lapuste <alexandru.lapuste@ROMOB41068>
2020-02-13 16:39:51 +0200

    #235 Change screenshot path and add it to test resul

--- Helpers/TestBase.cs
+++ Helpers/TestBase.cs
@@ -50,7 +50,7 @@ namespace RCoS
             cap.AddArgument("--incognito");
             driver = new ChromeDriver(cap);
             driver.Url = "https://rcs-cosmetics-client-dev.azurewebsites.net/";
-            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(60));
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
         public static void SwitchTab()
         {
